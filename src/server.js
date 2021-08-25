6ec879acfa058162c429f07a210cce2715ff36e6
Sunder Rawat <sunderrawat777@gmail.com>
2021-08-25 23:53:39 +0530

    handel uncauht excdptions

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 const app = require('./app')
 require("./db/mongoose")
 
-//create server and listen
-app.listen('3000', ()=>{
-    console.log('app is started on port 3000')
-})
+//define port for server
+const port = process.env.PORT || 3000;
+
+//catch uncaught exception error and shutting down server
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandeled Exception error 💥 shutting down server....💥");
+  process.exit(1);
+});
+
+//start server
+app.listen(port, () => {
+  console.log("app is listening on port ", port);
+});
+
+//catch unhandled rejection error and shuting down server
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandeled Rejection 💥 shutting down server....💥");
+  server.close(() => {
+    process.exit(1);
+  });
+});
